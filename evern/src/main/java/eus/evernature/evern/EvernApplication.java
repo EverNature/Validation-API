077bcb304d10bc78fdc274764c537fe6589b82cb
iarcos <izan.arcos@alumni.mondragon.edu>
2022-05-04 11:57:37 +0200

    Initial commit

---
+++ evern/src/main/java/eus/evernature/evern/EvernApplication.java
@@ -0,0 +1,13 @@
+package eus.evernature.evern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvernApplication.class, args);
+	}
+
+}
