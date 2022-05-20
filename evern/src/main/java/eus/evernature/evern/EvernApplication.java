fe68949ba424b18148a7686a18e3dcd1525d2cac
iarcos <izan.arcos@alumni.mondragon.edu>
2022-05-20 11:03:17 +0200

    model refactor

--- evern/src/main/java/eus/evernature/evern/EvernApplication.java
+++ evern/src/main/java/eus/evernature/evern/EvernApplication.java
@@ -14,33 +14,23 @@ public class EvernApplication {
 	}
 
 	@Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-    }
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	// Hola esto es una prueba
 
 	/**
 	 * @param expertService The service that will be used to save the user.
-	 * @param roleService The service that will be used to save the roles.
+	 * @param roleService   The service that will be used to save the roles.
 	 * @return A CommandLineRunner object.
 	 */
 	// @Bean
 	// CommandLineRunner run(ExpertService expertService, RoleService roleService) {
 	// 	return args -> {
 
-	// 		for(int i = 0; 1 < 100; i++) {
-	// 			byte[] array = new byte[7]; // length is bounded by 7
-	// 			new Random().nextBytes(array);
-	// 			String generatedString = new String(array, Charset.forName("UTF-8"));
-
-	// 			expertService.saveUser(new Expert(null, null, null, null, "izanarcos", "izan", "arcos", "izanarcos", "izan.arcos@gmail.com", "MU", "655699964", "+34", null, null));
-	// 		}
-
-
-	// 		expertService.addRoleToUser("izanarcos", "ROLE_SUPER_ADMIN");
-	// 		expertService.addRoleToUser("aritzdomaika", "ROLE_USER");
-	// 		expertService.addRoleToUser("jorgedasivla", "ROLE_MANAGER");
+	// 		expertService.saveUser(new Expert(null, null, null, null, "izanarcos", "izan", "arcos", "izanarcos",
+	// 				"izan.arcos@gmail.com", "MU", "655699964", "+34", null, null));
 	// 	};
 	// }
 }
--- evern/src/main/java/eus/evernature/evern/service/record/RecordServiceImpl.java
+++ evern/src/main/java/eus/evernature/evern/service/record/RecordServiceImpl.java
@@ -3,10 +3,8 @@ package eus.evernature.evern.service.record;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
